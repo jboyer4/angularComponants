857125b0185a43b1f12d805fd8af9f23915c08e0
Justin Boyer <justinbboyer@gmail.com>
2021-06-26 18:01:02 -0700

    Add skills and attributes

---
+++ src/app/charGen/character.ts
@@ -0,0 +1,32 @@
+export type Attributes = {
+    strength: number;
+    dexterity: number;
+    constitution: number;
+    intelligence: number;
+    wisdom: number;
+    charisma: number;
+}
+export type Skills = {
+    acrobatics: number;
+    animalHandling: number;
+    arcana: number;
+    athletics: number;
+    deception: number;
+    history: number;
+    insight: number;
+    intimidation: number;
+    investigation: number;
+    medicine: number;
+    nature: number;
+    perception: number;
+    performance: number;
+    persuasion: number;
+    religion: number;
+    slightOfHand: number;
+    stealth: number;
+    survival: number;
+}
+export class character {
+    public attributes: Attributes;
+
+}
