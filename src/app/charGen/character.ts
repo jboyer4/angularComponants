49eb1cc2ddc092c153e0b1cb02cc92a0bf069ebf
Justin Boyer <justinbboyer@gmail.com>
2021-06-26 18:18:38 -0700

    Make skills optional

--- src/app/charGen/character.ts
+++ src/app/charGen/character.ts
@@ -1,3 +1,4 @@
+
 export type Attributes = {
     strength: number;
     dexterity: number;
@@ -6,27 +7,35 @@ export type Attributes = {
     wisdom: number;
     charisma: number;
 }
+
 export type Skills = {
-    acrobatics: number;
-    animalHandling: number;
-    arcana: number;
-    athletics: number;
-    deception: number;
-    history: number;
-    insight: number;
-    intimidation: number;
-    investigation: number;
-    medicine: number;
-    nature: number;
-    perception: number;
-    performance: number;
-    persuasion: number;
-    religion: number;
-    slightOfHand: number;
-    stealth: number;
-    survival: number;
+    acrobatics?: number;
+    animalHandling?: number;
+    arcana?: number;
+    athletics?: number;
+    deception?: number;
+    history?: number;
+    insight?: number;
+    intimidation?: number;
+    investigation?: number;
+    medicine?: number;
+    nature?: number;
+    perception?: number;
+    performance?: number;
+    persuasion?: number;
+    religion?: number;
+    slightOfHand?: number;
+    stealth?: number;
+    survival?: number;
 }
 export class character {
     public attributes: Attributes;
+    public class: string;
+    public proficiencies: Skills;
+    public proficiencyBonus: number;
+    public maxhp: number;
+    constructor(attributes: Attributes, proficiencies: Skills, level) {
+        this.attributes = attributes;
 
+    }
 }
