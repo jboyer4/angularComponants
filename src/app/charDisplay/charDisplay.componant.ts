cce49171e355231dd9234787c09e119a1c202e4b
Justin Boyer <justinbboyer@gmail.com>
2021-06-26 18:48:46 -0700

    Create display component

---
+++ src/app/charDisplay/charDisplay.componant.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'char-display',
+    templateUrl: './charDisplay.component.html',
+    styleUrls: ['./charDisplay.component.css'],
+})
+export class charDisplay {
+
+}
